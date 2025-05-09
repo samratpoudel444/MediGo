693e9f1d8e323e879430a3b56f9dd58edc2d2be8
samratpoudel444 <poudelsamrat444@gmail.com>
2025-05-09 21:34:35 +0545

    added the database connection

---
+++ backend/server/helper/dbHelper.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config({ path: "../../.env" });
+
+const connectDB = async () => {
+  try {
+    const URL = process.env.MONGO_DB_URL;
+    await mongoose.connect(URL);
+    console.log("Server to Database");   
+
+  } catch (error) {
+    console.error("Connection failed");
+    console.error(error.message);
+    process.exit(1);
+  }
+};
+
+
+connectDB()
+//export default connectDB;
