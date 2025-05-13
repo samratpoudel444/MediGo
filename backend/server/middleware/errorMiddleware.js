728524704a23a138e90fed7f3cec6d2e28a00529
samratpoudel444 <poudelsamrat444@gmail.com>
2025-05-13 20:38:54 +0545

    added error middleWare

---
+++ backend/server/middleware/errorMiddleware.js
@@ -0,0 +1,9 @@
+
+
+export const errorHandler= async(err, req, res, next)=>
+{
+    if(err)
+    {
+        return res.status(err.code).json({message: err.message ||"Something Went Wrong" });
+    }
+}
