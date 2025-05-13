1bf96bb368661a89e620517655f67e1e94526e68
samratpoudel444 <poudelsamrat444@gmail.com>
2025-05-13 19:28:10 +0545

    fixed all errors

--- backend/server/helper/dbHelper.js
+++ backend/server/helper/dbHelper.js
@@ -6,15 +6,14 @@ const connectDB = async () => {
   try {
     const URL = process.env.MONGO_DB_URL;
     await mongoose.connect(URL);
-    console.log("Server to Database");   
-
+    console.log("Server to Database");
   } catch (error) {
     console.error("Connection failed");
+
     console.error(error.message);
     process.exit(1);
   }
 };
 
-
-connectDB()
+connectDB();
 //export default connectDB;
