ac0c090233b1d2a3f2fe1eb277804fa97ca683d2
Samrat Poudel <samrat.poudel@swifttech.com>
2025-05-15 17:01:44 +0545

    added the data

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,10 @@ import express from"express";
 import { errorHandler } from "./server/middleware/errorMiddleware.js";
 import dotenv from "dotenv";
 import authRouter from "./server/routes/authRoute/authRoute.js";
+import doctorRouter from "./server/routes/authRoute/doctorRoute.js";
 import connectDB from "./server/helper/dbHelper.js";
 import cookieParser from "cookie-parser";
+
 dotenv.config();
 
 
@@ -16,6 +18,7 @@ app.use(cookieParser())
 
 connectDB()
 app.use("/api/v1", authRouter);
+app.use("/api/v1", doctorRouter)
 app.use(errorHandler);
 
 
---
+++ backend/server/routes/authRoute/doctorRoute.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { getDoctorProfile } from "../../controllers/doctorController/getProfileDetails.js";
+const doctorRouter= express.Router();
+
+doctorRouter.route('/getDoctorProfile').get(getDoctorProfile);
+
+
+export default doctorRouter;
