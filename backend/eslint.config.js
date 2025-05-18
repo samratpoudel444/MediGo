cd310245038cb4745ccb2026a73e2f2318aa6554
Samrat Poudel <samrat.poudel@swifttech.com>
2025-05-18 10:52:57 +0545

    Configure ESLint with Prettier integration

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -1,6 +1,17 @@
 import { defineConfig } from "eslint/config";
-
+import prettierPlugin from 'eslint-plugin-prettier';
+import { ESLint } from "eslint/js";
 
 export default defineConfig([
-    
+    js.config.recommended,
+
+    {
+        files:[`**\*.js`],
+        plugins:{
+            prettier: prettierPlugin
+        },
+        rules:{
+            'prettier/prettier' :'error',
+        }
+    }
 ]);
