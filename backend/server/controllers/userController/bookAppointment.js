16deab53202d03d6bfc758b3cf39c4f9d319ffc1
samratpoudel444 <poudelsamrat444@gmail.com>
2025-05-21 22:28:03 +0545

    adding files

---
+++ backend/server/controllers/userController/bookAppointment.js
@@ -0,0 +1,11 @@
+// const bookAppointment= async(req, res, next)=>
+// {
+//     try{
+
+//     }
+//     catch(err)
+//     {
+//         console.log(err);
+//         return next({code:500, message:"Internal Server Error"});
+//     }
+// }
--- backend/server/routes/authRoute/userRoute.js
+++ backend/server/routes/authRoute/userRoute.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getUserDetails } from "../../controllers/userController/getUserDetails.js";
+import { getMyDetails } from "../../controllers/userController/getUserDetails.js";
+import { authMiddleware, isPatient } from "../../middleware/authMiddleware.js";
 
 const userRouter = express.Router();
 
-userRouter.route("/getUserDetails").get(getUserDetails);
+userRouter.route("/getMyDetails").get(authMiddleware, isPatient, getMyDetails); //to get your own details from middleware
 
 export default userRouter;
