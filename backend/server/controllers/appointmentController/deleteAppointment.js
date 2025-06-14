2db33cdad5c29de2f884270ba8f012a9e142263b
utsav789 <luintelutsav5@gmail.com>
2025-06-14 12:22:17 +0545

    fixed erros

--- backend/server/controllers/appointmentController/deleteAppointment.js
+++ backend/server/controllers/appointmentController/deleteAppointment.js
@@ -13,5 +13,6 @@ export const deleteAppointment = async (req, res) => {
 
 	await AppointmentTable.deleteOne({ _id: appointmentId });
 
+	console.log("drel")
 	return res.status(200).send({ message: "Appointment deleted Successfully" });
 };
