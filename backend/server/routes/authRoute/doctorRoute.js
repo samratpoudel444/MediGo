97a9b15c9c0171871c415e8d155c5ce5eeb8dc08
samratpoudel444 <poudelsamrat444@gmail.com>
2025-07-12 23:03:24 +0545

    added the frontends

--- backend/server/routes/authRoute/doctorRoute.js
+++ backend/server/routes/authRoute/doctorRoute.js
@@ -1,10 +1,14 @@
 import express from "express";
 import { getDetails } from "../../controllers/doctorController/getProfileDetails.js";
 import { authMiddleware, isDoctor } from "../../middleware/authMiddleware.js";
-
+import { getAllAppointments } from "../../controllers/doctorController/getAllAppointments.js";
+getAllAppointments
 
 const doctorRouter = express.Router();
 
 doctorRouter.route("/getDoctorProfile").get(authMiddleware, isDoctor, getDetails);
+doctorRouter
+  .route("/getAllDoctorAppointments")
+  .get(authMiddleware, isDoctor, getAllAppointments);
 
 export default doctorRouter;
